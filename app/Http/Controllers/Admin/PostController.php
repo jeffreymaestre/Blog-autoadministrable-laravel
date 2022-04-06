0ec6cfeba4d5150cbe16b5744ac9edf12b42b346
jeffreymaestre <36870162+jeffreymaestre@users.noreply.github.com>
2022-04-06 10:36:28 -0500

    Creacion de polices

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -82,6 +82,8 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+        $this->authorize('author', $post);
+
         $categories = Category::pluck('name', 'id');
         $tags = Tag::all();
 
@@ -97,6 +99,8 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
+        $this->authorize('author', $post);
+
         $post->update($request->all());
 
         if ($request->file('file')) {
@@ -129,6 +133,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        $this->authorize('author', $post);
+
         $post->delete();
 
         return redirect()->route('admin.posts.index')->with('info', 'El post se eliminó correctamente');
