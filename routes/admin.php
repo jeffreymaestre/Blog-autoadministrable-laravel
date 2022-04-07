93bd23609930dbed9453e1ed48d3696d1fc9a1c0
jeffreymaestre <36870162+jeffreymaestre@users.noreply.github.com>
2022-04-06 20:59:42 -0500

    Asignacion de roles a un usuario
    Se asigno roles a un usuario utilizando Laravel-Permission

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
+
+Route::resource('users', UserController::class)->only(['index', 'edit', 'update'])->names('admin.users');
 
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 
